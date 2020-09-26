5efd10e4d7672becfd47e22d9201656d8edd3395
Kudzai Lawrence Shava <41476928+klshava@users.noreply.github.com>
2020-09-26 14:59:41 +0200

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,39 @@ const cookieParser = require('cookie-parser')
 app.use(cookieParser())
 const bodyParser = require('body-parser')
 
+var port = process.env.PORT || 3000,
+    http = require('http'),
+    fs = require('fs'),
+    html = fs.readFileSync('index.html');
+
+var log = function(entry) {
+    fs.appendFileSync('/tmp/sample-app.log', new Date().toISOString() + ' - ' + entry + '\n');
+};
+
+var server = http.createServer(function (req, res) {
+    if (req.method === 'POST') {
+        var body = '';
+
+        req.on('data', function(chunk) {
+            body += chunk;
+        });
+
+        req.on('end', function() {
+            if (req.url === '/') {
+                log('Received message: ' + body);
+            } else if (req.url = '/scheduled') {
+                log('Received task ' + req.headers['x-aws-sqsd-taskname'] + ' scheduled at ' + req.headers['x-aws-sqsd-scheduled-at']);
+            }
+
+            res.writeHead(200, 'OK', {'Content-Type': 'text/plain'});
+            res.end();
+        });
+    } else {
+        res.writeHead(200);
+        res.write(html);
+        res.end();
+    }
+});
 app.use(session({
 
   secret: '%4$#kldfkalfg@@*)(new secrete)&&lkdsfls!',
